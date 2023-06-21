e7018cfbda92e8d1e0f64ef31a65b3b4840fac0f
EllaKaye <ella.kaye@gmail.com>
2023-06-21 16:23:12 +0100

    Add sumC documentation

--- src/sumC.cpp
+++ src/sumC.cpp
@@ -1,6 +1,11 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//' Add the Numbers in a Vector
+//'
+//' Return the sum of a vector of numbers.
+//' @param x A numeric vector
+//' @export
 // [[Rcpp::export]]
 double sumC(NumericVector x) {
   int n = x.size();
